cab9ee0483a764caa1be729245c0dbf8323ef9fe
sarifor <iread1q84@gmail.com>
2023-10-20 08:39:35 +0900

    Fix misc.

--- SQL_basics/04.sql
+++ SQL_basics/04.sql
@@ -4,5 +4,4 @@ SELECT * FROM zerocho.employee WHERE team = "dev" LIMIT 2; /* 처음부터 2개 
 SELECT * FROM zerocho.employee WHERE team = "dev" LIMIT 2 OFFSET 2; /* 처음부터 2개, 2번째 것부터 가져옴(0부터 시작) */
 
 /* cursor 방식 */
-/* 게시물 13, 12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 일 때 */
-SELECT * FROM zerocho.employee WHERE team = "dev" AND id < 9 LIMIT 5
+SELECT * FROM zerocho.employee WHERE team = "dev" AND id < 6 LIMIT 2;
